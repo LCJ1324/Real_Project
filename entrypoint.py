5e54554a286dda8b9c5a1469802ebeed25d4d2b6
LCJ1324 <chn4567@gmail.com>
2025-03-11 09:41:27 +0900

    Add files via upload

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 pages = [
-    st.Page("CE.py", title="전기 화학적 특성 분석", icon="🔍", default=True),
+    st.Page("CE.py", title="전기화학적 특성 분석", icon="🔍", default=True),
     st.Page("chemical.py", title="물리적 특성 분석", icon="🔬"),
     st.Page('predict_copy.py', title = '수명 예측', icon="⏳"),
     st.Page("hyper.py", title="머신러닝", icon="🤖")
